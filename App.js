dac9665e0059c5a1d16d26aa309972ff0f6ea79d
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-07 21:37:37 -0400

    Added Create Screen which will allow users to create new posts

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { createAppContainer } from 'react-navigation'
 import React from 'react'
 import IndexScreen from './src/Screens/IndexScreen'
 import ShowScreen from './src/Screens/ShowScreen'
+import CreateScreen from './src/Screens/CreateScreen'
 import { Provider } from './src/Context/BlogContext'
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
-  Show: ShowScreen
+  Show: ShowScreen,
+  Create: CreateScreen
 }, {
   initalRouteName: 'Index',
   defaultNavigationOptions: {
--- src/Screens/IndexScreen.js
+++ src/Screens/IndexScreen.js
@@ -33,6 +33,14 @@ export default function IndexScreen({ navigation }) {
     )
 }
 
+IndexScreen.navigationOptions =({ navigation }) => {
+    return {
+        headerRight: () => <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                                <Feather name="plus" size={30} />   
+                            </TouchableOpacity>
+    }
+}
+
 const styles = StyleSheet.create({
     row: {
         flexDirection: 'row',
